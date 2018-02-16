7a93f1b8eb60dfed54d3c74801ebc6016a415bbc
Prisedaka <prisedaka@ya.ru>
2018-02-17 01:37:00 +0300

    step 6 iter 1

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ start_calc:
 	npm run babel-node -- src/bin/brain-calc.js
 start_even:
 	npm run babel-node -- src/bin/brain-even.js
+start_gcd:
+	npm run babel-node -- src/bin/brain-gcd.js
 publish:
 	npm publish
 lint:
---
+++ src/games/gcd.js
@@ -0,0 +1,16 @@
+import { cons, car, cdr, toString } from 'hexlet-pairs';
+import { playGame, generator } from '..';
+
+const gcd = (num1, num2) => {
+  if (num2 === 0) return num1;
+  return gcd(num2, num1 % num2);
+};
+const gamePair = () => {
+  const randomNum1 = Math.floor(Math.random() * 10);
+  const randomNum2 = Math.floor(Math.random() * 10);
+  const correctAnswer = String(gcd(randomNum1, randomNum2));
+  return cons(`${randomNum1} ${randomNum2}`, correctAnswer);
+};
+const gameTask = 'Find the greatest common divisor of given numbers.';
+const start = () => playGame(gameTask, generator(gamePair));
+export default start;
